d3dfe71bcf281deedfe3792b56e01ff7c841a48a
Junior Holowka <junior.holowka@gmail.com>
2012-01-11 18:48:34 -0200

    app controller, apenas testes

---
+++ app/Controller/app_controller.php
@@ -0,0 +1,17 @@
+<?php
+
+class AppController extends Controller {
+
+    public $components = array(
+        'Session',
+        'Auth' => array(
+            'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
+            'logoutRedirect' => array('controller' => 'pages', 'action' => 'display', 'home')
+        )
+    );
+
+    function beforeFilter() {
+        $this->Auth->allow('index', 'view');
+    }
+
+}
