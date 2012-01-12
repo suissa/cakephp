3e47ab2ec22e78a085d5aa788587f61eb71c3016
Junior Holowka <junior.holowka@gmail.com>
2012-01-12 17:01:54 -0200

    Funcoes index() e view() adicionadas ao PostsController

--- app/Controller/posts_controller.php
+++ app/Controller/posts_controller.php
@@ -1,9 +1,16 @@
-<?
-class PostsController extends AppController{
- 
- public $name = 'Posts';
- 
-    var $scaffold;
-    
+<?php
+class PostsController extends AppController {
+	var $helpers = array ('Html','Form');
+	var $name = 'Posts';
+
+	function index() {
+		$this->set('posts', $this->Post->find('all'));
+	}
+	
+	function view($id = null) {
+		$this->Post->id = $id;   
+		$this->set('post', $this->Post->read());
+	}
+	
 }
 ?>
