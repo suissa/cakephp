168a6a8ff82b176dc920f05c7a8050429190dbf6
Jean Nascimento <jnascimento@gmail.com>
2012-01-12 17:34:05 -0200

    Criação de views para o profile

--- app/Controller/profile_controller.php
+++ app/Controller/profile_controller.php
@@ -2,57 +2,56 @@
 class ProfileController extends AppController{
  
     public $name = 'Profile'; 
-    var $scaffold;
-//    public function index(){
+//    var $scaffold;
+    public function index(){
 //	$posts = $this->Profile->find('all');
+	
+	$this->set('results', $this->Profile->find('all'));
 //	$this->set(compact('profile'));
-//    }
-//
-//    public function create(){
-//	$field = $this->data[$this->name];
-////	var_dump(is_string($field["name"]));
-////	die();
-//	if($this->data){
-//	    if(is_string($field["name"])){
-//		if($this->Profile->save($this->data))
-//		    $this->Session->setFlash('Cadastrado com sucesso!');
-//		$this->data = array();
-//	    }	
-//	    else{
-//		    $this->Session->setFlash('ERROR!');
-//		
-//	    }
-//	}
-//    }
-//    
-//    public function retrieve(){
-//     if($this->data){
-//      if($this->Profile->save($this->data))
-//       $this->Session->setFlash('Cadastrado com sucesso!');
-//      $this->data = array();
-//     }
-//    }
-//    
-//    public function update($id = null){
-//	if($this->data){
-//	    if($this->Profile->save($this->data))
-//		$this->Session->setFlash('Editado com sucesso!');
-//	    $this->redirect(array('controller' => 'profile','action' => 'index'));
-//	}
-//	else{
-//	    $this->data = $this->Profile->read(null,$id);
-//	}
-//    }
-//    public function delete($id = null){
-//	if(is_int($id)){
-//	    if($this->Profile->delete($id))
-//		$this->Session->setFlash('Deletado com sucesso!');
-//	    $this->redirect(array('controller' => 'profile','action' => 'index'));
-//	}
-//	elseif(is_string($id)){
-//	    //criar funcao de deletar pelo valor do name
-//	}
-//    }
+    }
+
+    public function create(){
+	$field = $this->data[$this->name];
+//	var_dump(is_string($field["name"]));
+//	die();
+	if($this->data){
+	    if(is_string($field["name"])){
+		if($this->Profile->save($this->data))
+		    $this->Session->setFlash('Cadastrado com sucesso!');
+		$this->data = array();
+	    }	
+	    else{
+		    $this->Session->setFlash('ERROR!');
+		
+	    }
+	}
+    }
+    
+    function view($id = null) {
+	$this->Profile->id = $id;   
+	$this->set('result', $this->Profile->read());
+    }
+    
+    public function update($id = null){
+	if($this->data){
+	    if($this->Profile->save($this->data))
+		$this->Session->setFlash('Editado com sucesso!');
+	    $this->redirect(array('controller' => 'profile','action' => 'index'));
+	}
+	else{
+	    $this->data = $this->Profile->read(null,$id);
+	}
+    }
+    public function delete($id = null){
+	if(is_int($id)){
+	    if($this->Profile->delete($id))
+		$this->Session->setFlash('Deletado com sucesso!');
+	    $this->redirect(array('controller' => 'profile','action' => 'index'));
+	}
+	elseif(is_string($id)){
+	    //criar funcao de deletar pelo valor do name
+	}
+    }
     
 }
 ?>
--- app/View/Profile/index.ctp
+++ app/View/Profile/index.ctp
@@ -1,8 +1,17 @@
-<html>
-<head>
-	<title>View do Profile</title>
-</head>
-<body>
-	<h1>Profiles</h1>
-</body>
-</html>
+<h1>Profiles</h1>
+<table>
+	<tr>
+		<th>Id</th>
+		<th>Profile</th>
+	</tr>
+
+	<?php foreach ($results as $result): ?>
+	<tr>
+		<td><?php echo $result['Profile']['id']; ?></td>
+		<td>
+			<?php echo $this->Html->link($result['Profile']['name'], array('controller' => 'profile', 'action' => 'view', $result['Profile']['id'])); ?>
+		</td>
+	</tr>
+	<?php endforeach; ?>
+
+</table>
