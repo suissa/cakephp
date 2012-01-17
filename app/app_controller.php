abe2db1e72f83a988f097357f202764130c4d559
Junior Holowka <junior.holowka@gmail.com>
2012-01-17 11:30:45 -0200

    Auth app controller

--- app/app_controller.php
+++ app/app_controller.php
@@ -1,34 +1,32 @@
 <?php
-/**
- * Application level Controller
- *
- * This file is application-wide controller file. You can put all
- * application-wide controller-related methods here.
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.app
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
+class AppController extends Controller 
+{
 
-/**
- * Application Controller
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @package       cake
- * @subpackage    cake.app
- */
-class AppController extends Controller {
+    var $components = array('Auth','Session');
+	var $current_user = false;
+
+    function beforeFilter()
+    {
+
+        $this->Auth->loginAction = array('controller' => 'sessions', 'action' => 'create');
+        $this->Auth->loginRedirect = '/';
+        $this->Auth->logoutRedirect = '/';
+
+        $this->Auth->authenticate = array(
+            AuthComponent::ALL => array(
+                'fields' => array(
+                    'username' => 'email',
+                    'password' => 'password'),
+                'userModel' => 'Users.User'
+            ), 'Form'
+        );
+
+        $this->current_user = $this->Auth->user();
+    }
+
+    function beforeRender()
+    {
+        $this->set('current_user',$this->current_user );
+    }
 }
+?>
