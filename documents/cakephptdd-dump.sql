572befd875742a5c7ae9a259b5286ea915ba39f3
Junior Holowka <junior.holowka@gmail.com>
2012-01-18 17:02:56 -0200

    Base remodelada

--- documents/cakephptdd-dump.sql
+++ documents/cakephptdd-dump.sql
@@ -12,20 +12,34 @@ CREATE TABLE `posts` (
 DROP TABLE IF EXISTS `profiles`;
 
 CREATE TABLE `profiles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL auto_increment,
   `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `users`;
+INSERT INTO `profiles` (`id`, `name`, `created`, `modified`) VALUES (1, 'Admin', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `profiles` (`id`, `name`, `created`, `modified`) VALUES (2, 'Member', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(50) NOT NULL,
-  `pass` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `dt_created` datetime NOT NULL,
-  `profile_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (profile_id) REFERENCES profiles(id) 
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `profile_id` int(10) unsigned NOT NULL,
+  `active` tinyint(1) unsigned NOT NULL,
+  `created` datetime,
+  `modified` datetime,
+  PRIMARY KEY  (`id`),
+  KEY `profile_id` (`profile_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+INSERT INTO `users` (`id`, `username`, `password`, `fname`, `lname`, `email`, `profile_id`, `active`, `created`, `modified`) 
+VALUES (1, 'gonow', 'gonow@123', 'Dev', 'Gonow', 'email@domail.com', 1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+INSERT INTO `users` (`id`, `username`, `password`, `fname`, `lname`, `email`, `profile_id`, `active`, `created`, `modified`) 
+VALUES (2, 'suporte', 'gonow@123', 'Suporte', 'Gonow', 'email@domail.com', 2, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
