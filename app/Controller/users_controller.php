d44aee8155eeb35c2fc09af550e1cec52eb51ae7
Junior Holowka <junior.holowka@gmail.com>
2012-01-18 17:12:32 -0200

    Componente Auth rodando em User

--- app/Controller/users_controller.php
+++ app/Controller/users_controller.php
@@ -5,6 +5,72 @@ class UsersController extends AppController{
     public $ext;  
     var $helpers = array('Html', 'Form', 'Session' );
     var $components = array('Auth');
+
+
+	function admin_index() {
+		$this->User->recursive = 0;
+		$this->set('users', $this->paginate());
+	}
+
+	function admin_view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash('Usuário inválido.');
+			$this->redirect(array('action'=>'index'), null, true);
+		}
+		$this->set('users', $this->User->read(null, $id));
+	}
+
+	function admin_edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash('Usuário inválido.');
+			$this->redirect(array('action'=>'index'), null, true);
+		}
+		if (!empty($this->data)) {
+			if ($this->User->save($this->data))
+			{
+				$this->Session->setFlash('Usuário salvo');
+				$this->redirect(array('action'=>'index'), null, true);
+			}
+			else
+			{
+				$this->Session->setFlash('O usuário não pode ser salvo. Por favor, tente de novo.');
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->User->read(null, $id);
+		}
+		$groups = $this->User->Profile->find('list');
+		$this->set(compact('profile'));
+	}
+
+	function admin_delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash('ID inválido para este usuário');
+			$this->redirect(array('action'=>'index'), null, true);
+		}
+		if ($this->User->del($id)) {
+			$this->Session->setFlash('User #'.$id.' deleted');
+			$this->redirect(array('action'=>'index'), null, true);
+		}
+	}
+
+	function admin_add() {
+		if (!empty($this->data)) {
+			$this->User->create();
+			if ($this->User->save($this->data))
+			{
+				$this->Session->setFlash('Usuário salvo');
+				$this->redirect(array('action'=>'index'), null, true);
+		} else 
+			{
+				$this->Session->setFlash('O usuário não pode ser salvo. Por favor, tente de novo.');
+			}
+		}
+		$groups = $this->User->Profile->find('list');
+		$this->set(compact('profile'));
+	}
+
+
     
     
 	function login()
@@ -22,7 +88,7 @@ class UsersController extends AppController{
 		//debug($this->User);		
     }
 
-       
+ /*      
     function view($id = null) {
 	$this->User->id = $id;   
 	if($this->User->id){
@@ -63,6 +129,8 @@ class UsersController extends AppController{
 		    //criar funcao de deletar pelo valor do name
 		}
     }
+
+*/
     
 }
 ?>
