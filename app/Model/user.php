592e7154dd172ebda81835e14f70657a07843f0b
Junior Holowka <junior.holowka@gmail.com>
2012-01-18 12:53:34 -0200

    Alguns testes de validações, incompleto ainda

--- app/Model/user.php
+++ app/Model/user.php
@@ -1,48 +1,72 @@
 <?php
-class User extends AppModel
-{
-    var $displayField = 'name';
-
-    var $validate = array(
-        'name' => array(
-            'name-not-empty-rule' => array('rule'=>'notEmpty','allowEmpty'=>false,'required'=>true)
-        ),
-        'email' => array(
-            'email'     => array('rule'=>'email','allowEmpty'=>false,'required'=>true),
-            'isUnique'  => array('rule'=>'isUnique'),
-            'notEmpty'  => array('rule'=>'notEmpty')
-        ),
-        'password' => array(
-            'minLength' => array('rule'=>array('minLength',5),'allowEmpty'=>false,'required'=>true),
-            'notEmpty'      => array('rule'=>'notEmpty')
-        ),
-        'password_confirm' => array(
-            'comparePasswords'  => array('rule'=>array('comparePasswords','password'),'allowEmpty'=>false,'required'=>true)
-        )
-    );
-
-    function comparePasswords( $field=array(), $compare_field=null )
-    {
-        foreach( $field as $key => $value )
-        { 
-            $pass_1 = $value; 
-            $pass_2 = $this->data[$this->name][ $compare_field ];
-
-            if($pass_1 !== $pass_2) { 
-                    return false; 
-            } else { 
-                continue; 
-            } 
-        } 
-        return true;
-    }
-
-    function beforeSave( $options = array() )
-    {
-        $password_unhashed = $this->data[$this->name]['password'];
-        $this->data[$this->name]['password'] = AuthComponent::password( $password_unhashed );
-
-        return true;
-    }
+class User extends AppModel {
+	public $name = 'User';
+	//public $displayField = 'login';
+	
+	var $validate = array(
+			'login' => VALID_NOT_EMPTY,
+	        'pass' => VALID_NOT_EMPTY,
+			'email' => VALID_EMAIL,
+	);
+	
+/*	
+	public $validate = array(
+// 		'name'=>array(
+// 			'Please enter your name.'=>array(
+// 				'rule'=>'notEmpty',
+// 				'message'=>'Por favor, informe o seu nome'
+// 			)
+// 		),
+		'login'=>array(
+			'The username must be between 5 and 15 characters.'=>array(
+				'rule'=>array('between', 5, 15),
+				'message'=>'Seu login deverá ter de 5 a 15 caracteres'
+			),
+			'That username has already been taken'=>array(
+				'rule'=>'isUnique',
+				'message'=>'Este login já existe no sistema'
+			)
+		),
+		'email'=>array(
+			'Valid email'=>array(
+				'rule'=>array('email'),
+				'message'=>'Por favor, entre com um endereço de email válido'
+			)
+		),
+		'pass'=>array(
+		    'Not empty'=>array(
+		        'rule'=>'notEmpty',
+		        'message'=>'Por favor, informe a sua senha'
+		    ),
+		    'Match passwords'=>array(
+		        'rule'=>'matchPasswords',
+		        'message'=>'Suas senhas não conferem'
+		    )
+		),
+		'password_confirmation'=>array(
+		    'Not empty'=>array(
+		        'rule'=>'notEmpty',
+		        'message'=>'Por favor, confirme a sua senha'
+		    )
+		)
+	);
+	
+	public function matchPasswords($data) {
+	    if ($data['pass'] == $this->data['User']['password_confirmation']) {
+	        return true;
+	    }
+	    $this->invalidate('password_confirmation', 'Your passwords do not match');
+	    return false;
+	}
+	
+	public function beforeSave() {
+	    if (isset($this->data['User']['pass'])) {
+	        $this->data['User']['pass'] = AuthComponent::password($this->data['User']['pass']);
+	    }
+	    return true;
+	}
+	
+*/
+	
 }
 ?>
