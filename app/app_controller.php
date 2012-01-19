ca20188cf1d53f731355a2114909617894df3d05
Junior Holowka <junior.holowka@gmail.com>
2012-01-19 17:43:09 -0200

    Configurações de admin utilizando Auth

--- app/app_controller.php
+++ app/app_controller.php
@@ -13,9 +13,12 @@ class AppController extends Controller {
 	
 	function beforeFilter() {
 		$this->Auth->allow('index', 'view');
+		$this->Auth->authError = __('Desculpe, é necessário logar no sistema.', true);
+		$this->Auth->loginError = __('Email/senha inválida, tente novamente', true);
+		$this->Auth->userScope = array('User.active' => 1);
+		
 	}
 	
-
 // 	function isAuthorized() {
 // 		if (isset($this->params[Configure::read('Routing.admin')])) {
 // 			if ($this->Auth->user('profile_id') != 1) {
@@ -23,7 +26,7 @@ class AppController extends Controller {
 // 			}
 // 		}
 // 		return true;
-//    } 
-
+// 	}
+	
 }
 ?>
