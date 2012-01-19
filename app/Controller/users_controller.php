c8156951728cfdd141462cd5007b773281201a4b
Junior Holowka <junior.holowka@gmail.com>
2012-01-19 17:41:12 -0200

    Trabalhando com area restrita

--- app/Controller/users_controller.php
+++ app/Controller/users_controller.php
@@ -5,10 +5,74 @@ class UsersController extends AppController{
     public $ext;  
     var $helpers = array('Html', 'Form', 'Session' );
     var $components = array('Auth');
+    
+    
+    function admin_index() {
+    $this->User->recursive = 0;
+    $this->set('users', $this->paginate());
+    }
+    
+    function admin_view($id = null) {
+    if (!$id) {
+    $this->Session->setFlash('Usuário inválido.');
+    			$this->redirect(array('action'=>'index'), null, true);
+    }
+    $this->set('user', $this->User->read(null, $id));
+    }
+    
+    function admin_edit($id = null) {
+    if (!$id && empty($this->data)) {
+    $this->Session->setFlash('Usuário inválido.');
+    $this->redirect(array('action'=>'index'), null, true);
+    }
+    		if (!empty($this->data)) {
+    if ($this->User->save($this->data))
+    {
+    $this->Session->setFlash('Usuário salvo');
+    $this->redirect(array('action'=>'index'), null, true);
+    }
+    			else
+    {
+    				$this->Session->setFlash('O usuário não pode ser salvo, tente novamente');
+    			}
+    		}
+    		if (empty($this->data)) {
+    			$this->data = $this->User->read(null, $id);
+    }
+    $groups = $this->User->Profile->find('list');
+    $this->set(compact('profiles'));
+    }
+    
+    function admin_delete($id = null) {
+    		if (!$id) {
+    $this->Session->setFlash('Invalid id for User');
+    $this->redirect(array('action'=>'index'), null, true);
+    }
+    if ($this->User->del($id)) {
+    $this->Session->setFlash('User #'.$id.' deleted');
+    $this->redirect(array('action'=>'index'), null, true);
+    		}
+    	}
+    
+    function admin_add() {
+    if (!empty($this->data)) {
+    $this->User->create();
+    if ($this->User->save($this->data))
+    {
+    $this->Session->setFlash('The User has been saved');
+    $this->redirect(array('action'=>'index'), null, true);
+    		} else 
+    			{
+    				$this->Session->setFlash('The User could not be saved. Please, try again.');
+    }
+    }
+    $groups = $this->User->Profile->find('list');
+    		$this->set(compact('profiles'));
+    	}
 
 	public function beforeFilter() {
 	    parent::beforeFilter();
-	    $this->Auth->allow('save');
+	    $this->Auth->allow('login');
 	}
 	
 
